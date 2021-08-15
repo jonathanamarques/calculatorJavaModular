baa4d9aad04df8833e80b732ad9ff02f83f427ad
jonathanamarques <jonathanamarques@gmail.com>
2021-08-15 15:33:23 -0300

    Primeiro commit

---
+++ digital.innovation.one.core/src/digital/innovation/one/core/Runner.java
@@ -0,0 +1,12 @@
+package digital.innovation.one.core;
+
+import digital.innovation.one.utils.Calculadora;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(Calculadora.sum(1, 2));
+
+    }
+}
